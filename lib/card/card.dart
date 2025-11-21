9c67ca1d1245458394f4ecc51d7c31268e5808bc
RIOKOWI <achyarrio2004@gmail.com>
2025-11-21 11:25:09 +0700

    buat card

--- lib/card/card.dart
+++ lib/card/card.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
-class Card extends StatelessWidget {
-  const Card({super.key});
+class MyCard extends StatelessWidget {
+  const MyCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+            appBar: AppBar(
+              title: const Text("Card"),
+              centerTitle: true,
+              actions: const [],
+            ),
+            body: Center(
+              child: Container(
+                child: Column(
+                  children: [
+                    Card(
+                      color: Colors.red,
+                      child: Text(
+                        'Card 1',
+                        
+                      ),
+                    ),
+                    
+                  ],
+                ),
+              ),
+            ),
+          );
   }
 }
